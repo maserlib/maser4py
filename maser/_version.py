bddb779b08a54c9260525a08a5e81e9ae50343ec
xbonnin <xavier.bonnin@obspm.fr>
2016-10-21 15:21:50 +0200

    prepare 0.4.3

--- maser/_version.py
+++ maser/_version.py
@@ -3,7 +3,7 @@
 
 """maser._version."""
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __date__ = "21-OCT-2016"
 __change__ = {"0.1.0": "First beta release",
 "0.2.0": "Modify the source code tree",
@@ -16,4 +16,5 @@ __change__ = {"0.1.0": "First beta release",
 "0.3.0": "Add utils.cdf.converter.tools",
 "0.4.0": "Add solo.rpw module, update cdf.converter.tools.xlsx",
 "0.4.1": "Update README.rst, remove INSTALL.rst, update doc",
-"0.4.2": "Fix a bug in utils.cdf"}
+"0.4.2": "Fix a bug in utils.cdf",
+"0.4.3": "Update setup.py and requirements.txt"}
