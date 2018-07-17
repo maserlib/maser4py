af3d7082b7c82468091e4495a017621006bf64b7
Quynh Nhu NGUYEN <Quynh-Nhu.Nguyen@obspm.fr>
2018-07-17 15:33:40 +0200

    Import cdf_compare from cdf_compare instead of compare_cdf_files.

--- maser/utils/cdf/__init__.py
+++ maser/utils/cdf/__init__.py
@@ -5,4 +5,4 @@ from .converter import skeletoncdf, SkeletonCDFException
 from .validator import Validate
 from .tools import get_cdftype, get_vattrs, \
     get_cdftypename, get_numpttype
-from .cdfcompare.compare_cdf_files import cdf_compare
+from .cdfcompare.cdf_compare import cdf_compare
