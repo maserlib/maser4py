3b6a5374264333a81be65150214ddeeeede72f90
Cecconi Baptiste <baptiste.cecconi@obspm.fr>
2019-03-14 19:18:13 +0100

    added comments

--- maser/data/pds/__init__.py
+++ maser/data/pds/__init__.py
@@ -23,20 +23,47 @@ from pathlib import Path
 from maser.data.pds.const import PDS_OBJECT_CLASSES
 
 
-def load_pds_from_label(input_var, fmt_label_dict=None, verbose=False, debug=False, **kwargs):
+def load_pds_from_label(label_path=None, label_dict=None, fmt_label_dict=None, verbose=False, debug=False, **kwargs):
+    """This function loads the data associated with the provided PDS3 label file.
+
+    One of the :code:`label_path` or :code:`label_dict` input parameter must be set, or a :code:`ValueError` is raised.
+    
+    * The :code:`label_path` must be either a simple path (:code:`str`) or a :code:`pathlib.Path` object. If the 
+    parameter type is something else, a :code:`TypeError` is raised.
+    * The :code:`label_dict` must be a :code:`PDSLabelDict` object. If the parameter type is something else, a 
+    :code:`TypeError` is raised.
+    
+    The current list of available PDS3 data collections that have been tested are:
+    {}
+
+    :param label_path: Path to the label file (either a simple string or a pathlib.Path object)
+    :param label_dict: PDSLabelDict object (already loaded from label file)
+    :param fmt_label_dict: Dict with list of extra format label files (key=file name, value=file path)
+    :param verbose: Verbose mode (default is False)
+    :param debug: Debug mode (default is False)
+    :param kwargs: any other keywords will be passed to the data object.
+    :return: a PDSDataFromFile object.
+    """.format("".join(['* {}\n'.format(item) for item in PDS_OBJECT_CLASSES.keys()]))
 
     from maser.data.pds.classes import PDSLabelDict
 
-    if isinstance(input_var, str):
-        file = input_var
-        label = PDSLabelDict(input_var, fmt_label_dict=fmt_label_dict, verbose=verbose, debug=debug)
-    elif isinstance(input_var, Path):
-        file = str(input_var)
-        label = PDSLabelDict(file, fmt_label_dict=fmt_label_dict, verbose=verbose, debug=debug)
-    elif isinstance(input_var, PDSLabelDict):
-        label = input_var
-        file = label.file
-    else:
-        raise TypeError()
+    if label_path is not None or label_dict is not None:
 
-    return PDS_OBJECT_CLASSES[label['DATA_SET_ID']](file, label_dict=label, verbose=verbose, debug=debug, **kwargs)
+        if isinstance(label_path, str):
+            file = label_path
+            label = PDSLabelDict(label_path, fmt_label_dict=fmt_label_dict, verbose=verbose, debug=debug)
+        elif isinstance(label_path, Path):
+            file = str(label_path)
+            label = PDSLabelDict(file, fmt_label_dict=fmt_label_dict, verbose=verbose, debug=debug)
+        elif isinstance(label_dict, PDSLabelDict):
+            label = label_dict
+            file = label.file
+        else:
+            TypeError()
+
+        return PDS_OBJECT_CLASSES[label['DATA_SET_ID']](file, label_dict=label, verbose=verbose, debug=debug, **kwargs)
+
+    else:
+
+        raise ValueError()
+
