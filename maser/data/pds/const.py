a9797846537d9e883d6e7b2cf6ea93a6426e76df
Cecconi Baptiste <baptiste.cecconi@obspm.fr>
2019-03-14 18:51:26 +0100

    dispatched PDS_OBJECT_CLASSES

--- maser/data/pds/const.py
+++ maser/data/pds/const.py
@@ -1,24 +1,22 @@
-from maser.data.pds.cassini.rpws.wbr import PDSPPICassiniRPWSWBRFullResDataFromLabel
-from maser.data.pds.cassini.rpws.hfr import PDSPPICassiniRPWSHFRLowRateFullDataFromLabel
-from maser.data.pds.voyager.pra import PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel, \
-    PDSPPIVoyagerPRAHighRateDataTimeSeriesDataFromLabel
+from maser.data.pds.cassini.const import PDS_OBJECT_CLASSES as PDS_OBJECT_CLASSES_CASSINI
+from maser.data.pds.voyager.const import PDS_OBJECT_CLASSES as PDS_OBJECT_CLASSES_VOYAGER
+from maser.data.psa.mex.const import PDS_OBJECT_CLASSES as PDS_OBJECT_CLASSES_MARSEXPRESS
+import astropy.units as u
 
-__all__ = ['PDS_OBJECT_CLASSES']
+__all__ = ['PDS_OBJECT_CLASSES', 'PDS_UNITS']
 
-PDS_OBJECT_CLASSES = {
-    'CO-V/E/J/S/SS-RPWS-2-REFDR-WBRFULL-V1.0': PDSPPICassiniRPWSWBRFullResDataFromLabel,
-    'CO-V/E/J/S/SS-RPWS-3-RDR-LRFULL-V1.0': PDSPPICassiniRPWSHFRLowRateFullDataFromLabel,
-    'VG1-J-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG1-S-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-J-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-S-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-N-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-U-PRA-3-RDR-LOWBAND-6SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG1-J-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG1-S-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-J-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-S-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-N-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-U-PRA-4-SUMM-BROWSE-48SEC-V1.0': PDSPPIVoyagerPRARDRLowBand6SecDataFromLabel,
-    'VG2-N-PRA-2-RDR-HIGHRATE-60MS-V1.0': PDSPPIVoyagerPRAHighRateDataTimeSeriesDataFromLabel,
+PDS_OBJECT_CLASSES = {}
+PDS_OBJECT_CLASSES.update(PDS_OBJECT_CLASSES_CASSINI)
+PDS_OBJECT_CLASSES.update(PDS_OBJECT_CLASSES_VOYAGER)
+PDS_OBJECT_CLASSES.update(PDS_OBJECT_CLASSES_MARSEXPRESS)
+
+PDS_UNITS = {
+    'HERTZ': u.Unit('Hz'),
+    'HZ': u.Unit('Hz'),
+    'KILOHERTZ': u.Unit('kHz'),
+    'MILLIBELL': u.def_unit('milliBell', 0.01 * u.dB),
+    'N/A': u.dimensionless_unscaled,
+    'NANOTESLA**2/HZ': u.Unit('nT**2/Hz'),
+    'SECOND': u.s,
+    'VOLT**2/M**2/HZ': u.Unit('V**2/(m**2 Hz)'),
 }
