ddf833dc1df26fc87dbcb7394f9475ee98d231e2
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2022-02-26 18:41:15 +0100

    add nenupy optional dependency and use it for reading times and frequencies

--- maser/data/data.py
+++ maser/data/data.py
@@ -190,7 +190,41 @@ class SrnNdaRoutineJupEdrCdfData(CdfData, dataset="srn_nda_routine_jup_edr"):
 
 
 class NenufarBstFitsData(FitsData, dataset="nenufar_bst"):
-    pass
+    @classmethod
+    def _nenupy_open(cls, filepath):
+        from nenupy.beamlet import BST_Data
+
+        return BST_Data(str(filepath))
+
+    def __init__(
+        self,
+        filepath: Path,
+        dataset: Union[None, str] = "__auto__",
+        access_mode: str = "sweeps",
+        beam: int = 0,
+    ):
+        super().__init__(filepath, dataset, access_mode)
+        data = NenufarBstFitsData._nenupy_open(self.filepath)
+        if beam <= len(data.dbeams):
+            self.beam = beam
+        else:
+            raise ValueError(f"Beam #{beam} doesn't exist.")
+
+    @property
+    def times(self):
+        if self._times is None:
+            bst = NenufarBstFitsData._nenupy_open(self.filepath)
+            bst.dbeam = self.beam
+            self._times = bst.time
+        return self._times
+
+    @property
+    def frequencies(self):
+        if self._frequencies is None:
+            bst = NenufarBstFitsData._nenupy_open(self.filepath)
+            bst.dbeam = self.beam
+            self._frequencies = bst.freqs
+        return self._frequencies
 
 
 class ECallistoFitsData(FitsData, dataset="ecallisto"):
--- tests/test_data.py
+++ tests/test_data.py
@@ -128,10 +128,49 @@ def test_nenufar_bst_dataset():
         assert isinstance(data, NenufarBstFitsData)
 
 
+def test_nenufar_bst_dataset__beam():
+    for filepath in TEST_FILES["nenufar_bst"]:
+        data = Data(filepath=filepath, beam=1)
+        assert data.beam == 1
+
+
+def test_nenufar_bst_dataset__beam__value_error():
+    for filepath in TEST_FILES["nenufar_bst"]:
+        with pytest.raises(ValueError):
+            Data(filepath=filepath, beam=1000)
+
+
 def test_nenufar_bst_dataset__access_mode_file():
     for filepath in TEST_FILES["nenufar_bst"]:
         with Data(filepath=filepath, access_mode="file") as data:
             assert isinstance(data, fits.hdu.hdulist.HDUList)
+
+
+def test_nenufar_bst_dataset__times():
+    for filepath in TEST_FILES["nenufar_bst"]:
+        with Data(filepath=filepath) as data:
+            assert isinstance(data.times, Time)
+            assert len(data.times) == 3600
+            assert data.times[0] == Time(2459609.9792824076, format="jd")
+            assert data.times[-1] == Time(2459610.0209375, format="jd")
+
+
+def test_nenufar_bst_dataset__times__other_beam():
+    for filepath in TEST_FILES["nenufar_bst"]:
+        with Data(filepath=filepath, beam=1) as data:
+            assert isinstance(data.times, Time)
+            assert len(data.times) == 3600
+            assert data.times[0] == Time(2459609.9792824076, format="jd")
+            assert data.times[-1] == Time(2459610.0209375, format="jd")
+
+
+def test_nenufar_bst_dataset__frequencies():
+    for filepath in TEST_FILES["nenufar_bst"]:
+        with Data(filepath=filepath) as data:
+            assert isinstance(data.frequencies, Quantity)
+            assert len(data.frequencies) == 192
+            assert data.frequencies[0] == 25 * Unit("MHz")
+            assert data.frequencies[-1].value == pytest.approx(62.304688)
 
 
 def test_ecallisto_dataset():
