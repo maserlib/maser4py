6f8962d2e6a87fb99d679b6343916d2b91c96030
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2022-03-14 18:15:20 +0100

    use records access_mode for viking_v4n_e5 dataset

--- maser/data/cdpp/viking/data.py
+++ maser/data/cdpp/viking/data.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
+from typing import Union
 from maser.data.base import BinData
 from .records import VikingV4nE5Records
 
 
 class VikingV4nE5BinData(BinData, dataset="viking_v4n_e5"):
     _iter_sweep_class = VikingV4nE5Records
+    _access_modes = ["file", "records"]
+
+    def __init__(
+        self,
+        filepath: Path,
+        dataset: Union[None, str] = "__auto__",
+        access_mode: str = "records",
+        load_data: bool = True,
+    ) -> None:
+        super().__init__(filepath, dataset, access_mode, load_data)
+
+    @property
+    def sweeps(self):
+        raise ValueError("Illegal access mode.")
