ead90939eb7e99a403c24be19e6338be093a1762
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2022-05-04 15:56:35 +0200

    add __init__() class and frequencies property

--- maser/data/base/sweeps.py
+++ maser/data/base/sweeps.py
@@ -3,9 +3,13 @@ from .records import Records, Record
 
 
 class Sweep(Record):
+    def __init__(self, header, data):
+        super().__init__(header, data)
+        self._frequencies = None
+
     @property
     def frequencies(self):
-        pass
+        return self._frequencies
 
 
 class Sweeps(Records):
