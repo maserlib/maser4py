ffb1a21bef20e0607fc9a6ebea7128439643d67d
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2022-08-23 10:29:02 +0200

    Improve Cassini Kronos support (level=n1, as_xarray)

--- src/maser_data/maser/data/padc/cassini/data.py
+++ src/maser_data/maser/data/padc/cassini/data.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from typing import Union
 
-from maser.data.base import BinData, Sweeps
-from maser.data.base.records import Records
+from maser.data.base import BinData, Sweeps, Records
+from maser.data.base.sweeps import Sweep
+from .kronos import fi_freq, ti_datetime
 
-# from astropy.units import Unit
-# from astropy.time import Time
+from astropy.units import Unit
+from astropy.time import Time
 import numpy
 import json
 from pathlib import Path
@@ -18,6 +19,11 @@ with open(KRONOS_LEVEL_FORMAT_JSON_FILE, "r") as f:
 
 
 class Kronos:
+    # placeholder attributes
+    level = None
+    file_size = 0
+    filepath = Path()
+
     @property
     def _format(self):
         return kronos_level_format[self.level]
@@ -41,23 +47,37 @@ class Kronos:
         return data
 
 
+class CoRpwsHfrKronosN1DataSweep(Sweep):
+    def __init__(self, header, data):
+        super().__init__(header, data)
+        self._frequencies = header["frequencies"]
+        self._time = header["time"]
+
+
 class CoRpwsHfrKronosN1DataSweeps(Sweeps):
     @property
     def generator(self):
-        for sweep_mask in self.data_reference._sweep_masks:
-            yield self.data_reference_data[sweep_mask]
+        for f, t, sweep_mask in zip(
+            self.data_reference.frequencies,
+            self.data_reference.times,
+            self.data_reference._sweep_masks,
+        ):
+            yield CoRpwsHfrKronosN1DataSweep(
+                {
+                    "frequencies": f,
+                    "time": t,
+                    "level": self.data_reference.level,
+                    "file": self.data_reference.filepath.name,
+                },
+                self.data_reference._data[sweep_mask],
+            )
 
 
 class CoRpwsHfrKronosDataRecords(Records):
     @property
     def generator(self):
-        for row in self.data_reference._data:
-            yield dict(
-                zip(
-                    self.data_reference._format["record_def"]["fields"],
-                    row,
-                )
-            )
+        for i in range(self.data_reference._nrecord):
+            yield self.data_reference._data[i]
 
 
 class CoRpwsHfrKronosN1Data(BinData, Kronos, dataset="co_rpws_hfr_kronos_n1"):
@@ -80,6 +100,7 @@ class CoRpwsHfrKronosN1Data(BinData, Kronos, dataset="co_rpws_hfr_kronos_n1"):
         self.level = "n1"
         self._data = self.read_data_binary()
         self._sweep_masks = None
+        self.__max_frequencies_len = None
         self._nrecord = len(self._data)
         self._nsweep = len(self.sweep_masks)
 
@@ -100,6 +121,79 @@ class CoRpwsHfrKronosN1Data(BinData, Kronos, dataset="co_rpws_hfr_kronos_n1"):
             return self._nrecord
         else:
             return self.file_size
+
+    @property
+    def times(self):
+        if self._times is None:
+            times = Time(
+                list(
+                    map(
+                        ti_datetime,
+                        self._data[
+                            "ti"
+                        ],  # time index (YYDDDSSSSS) with YY = YYYY - 1996
+                        self._data["c"],  # centiseconds
+                    )
+                )
+            )
+            if self.access_mode == "records":
+                self._times = times
+            elif self.access_mode == "sweeps":
+                self._times = Time([times[mask][0] for mask in self.sweep_masks])
+        return self._times
+
+    @property
+    def frequencies(self):
+        if self._frequencies is None:
+            if self.access_mode == "records":
+                self._frequencies = numpy.array(
+                    list(map(fi_freq, self._data["fi"]))
+                ) * Unit("kHz")
+            if self.access_mode == "sweeps":
+                self._frequencies = [
+                    numpy.array(list(map(fi_freq, self._data[mask]["fi"])))
+                    * Unit("kHz")
+                    for mask in self.sweep_masks
+                ]
+        return self._frequencies
+
+    @property
+    def _max_frequencies_len(self):
+        if self.__max_frequencies_len is None:
+            self.__max_frequencies_len = numpy.max(
+                [numpy.count_nonzero(mask) for mask in self.sweep_masks]
+            )
+        return self.__max_frequencies_len
+
+    def as_xarray(self):
+        import xarray
+
+        data_arr = numpy.full((self._nsweep, self._max_frequencies_len), numpy.nan)
+        freq_arr = numpy.full((self._nsweep, self._max_frequencies_len), numpy.nan)
+
+        for i in range(self._nsweep):
+            f = self.frequencies[i].value
+            freq_arr[i, : len(f)] = f
+
+        freq_index = range(self._max_frequencies_len)
+
+        datasets = {}
+        for dataset_key in self._format["record_def"]["fields"]:
+            for i, sweep in enumerate(self.sweeps):
+                d = sweep.data[dataset_key]
+                data_arr[i, : len(d)] = d
+            datasets[dataset_key] = xarray.DataArray(
+                data=data_arr,
+                name=dataset_key,
+                coords={
+                    "freq_index": freq_index,
+                    "time": self.times.to_datetime(),
+                    "frequency": (["time", "freq_index"], freq_arr),
+                },
+                dims=("time", "freq_index"),
+            )
+
+        return datasets
 
 
 class CoRpwsHfrKronosN2Data(BinData, dataset="co_rpws_hfr_kronos_n2"):
--- tests/test_padc_kronos_n1.py
+++ tests/test_padc_kronos_n1.py
@@ -5,8 +5,9 @@ from maser.data import Data
 from maser.data.base import BinData
 from maser.data.padc.cassini.data import CoRpwsHfrKronosN1Data
 
-# from astropy.time import Time
-# from astropy.units import Quantity, Unit
+from astropy.time import Time
+from astropy.units import Quantity, Unit
+
 # import xarray
 
 TEST_FILES = {
@@ -25,26 +26,81 @@ def test_co_rpws_hfr_kronos_n1_bin_dataset():
         data = Data(filepath=filepath)
         assert isinstance(data, BinData)
         assert isinstance(data, CoRpwsHfrKronosN1Data)
+        assert data.level == "n1"
 
 
 @pytest.mark.test_data_required
 def test_co_rpws_hfr_kronos_n1_bin_dataset__len():
     filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
     data = Data(filepath=filepath)
-    assert len(data) == 219
+    assert data._nsweep == 219
     assert data._nrecord == 78621
     assert data.file_size == 2201388
 
 
 @pytest.mark.test_data_required
 def test_co_rpws_hfr_kronos_n1_bin_dataset__len_records():
-    filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
-    data = Data(filepath=filepath, access_mode="records")
-    assert len(data) == 78621
+    for filepath in TEST_FILES["co_rpws_hfr_kronos_n1"]:
+        data = Data(filepath=filepath, access_mode="records")
+        assert len(data) == data._nrecord
+
+
+@pytest.mark.test_data_required
+def test_co_rpws_hfr_kronos_n1_bin_dataset__len_sweeps():
+    for filepath in TEST_FILES["co_rpws_hfr_kronos_n1"]:
+        data = Data(filepath=filepath, access_mode="sweeps")
+        assert len(data) == data._nsweep
 
 
 @pytest.mark.test_data_required
 def test_co_rpws_hfr_kronos_n1_bin_dataset__len_file():
+    for filepath in TEST_FILES["co_rpws_hfr_kronos_n1"]:
+        data = Data(filepath=filepath, access_mode="file")
+        assert len(data) == data.file_size
+
+
+@pytest.mark.test_data_required
+def test_co_rpws_hfr_kronos_n1_bin_dataset__times_sweeps():
     filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
-    data = Data(filepath=filepath, access_mode="file")
-    assert len(data) == 2201388
+    data = Data(filepath=filepath)
+    assert len(data.times) == data._nsweep
+    assert isinstance(data.times, Time)
+    assert data._data["c"][0] == data.times[0].to_datetime().microsecond / 10000
+    assert data._data["c"][-1] == data.times[-1].to_datetime().microsecond / 10000
+    assert data.times[0] == Time("2012-06-29T20:00:01.320")
+    assert data.times[200] == Time("2012-06-29T20:54:57.300")
+    assert data.times[-1] == Time("2012-06-29T20:59:45.300")
+
+
+@pytest.mark.test_data_required
+def test_co_rpws_hfr_kronos_n1_bin_dataset__times_records():
+    filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
+    data = Data(filepath=filepath, access_mode="records")
+    assert len(data.times) == data._nrecord
+    assert isinstance(data.times, Time)
+    assert data.times[0] == Time("2012-06-29T20:00:01.320")
+    assert data.times[200] == Time("2012-06-29T20:00:01.320")
+    assert data.times[2000] == Time("2012-06-29T20:01:37.320")
+    assert data.times[-1] == Time("2012-06-29T20:59:45.300")
+
+
+@pytest.mark.test_data_required
+def test_co_rpws_hfr_kronos_n1_bin_dataset__frequencies_sweeps():
+    filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
+    data = Data(filepath=filepath)
+    assert len(data.times) == data._nsweep
+    assert isinstance(data.frequencies, list)
+    assert isinstance(data.frequencies[0], Quantity)
+    assert data.frequencies[0][0] == 3.6856 * Unit("kHz")
+    assert data.frequencies[-1][0] == 3.6856 * Unit("kHz")
+    assert data.frequencies[-1][-1] == 16025 * Unit("kHz")
+
+
+@pytest.mark.test_data_required
+def test_co_rpws_hfr_kronos_n1_bin_dataset__frequencies_records():
+    filepath = TEST_FILES["co_rpws_hfr_kronos_n1"][0]
+    data = Data(filepath=filepath, access_mode="records")
+    assert len(data.times) == data._nrecord
+    assert isinstance(data.frequencies, Quantity)
+    assert data.frequencies[0] == 3.6856 * Unit("kHz")
+    assert data.frequencies[-1] == 16025 * Unit("kHz")
