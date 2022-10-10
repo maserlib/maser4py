831dbc173d5aa951cc78cc5720fe45d5b7e3b82b
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2022-10-10 11:01:42 +0200

    Add as_xarray() for NDA/Routine (CDF)

--- src/maser_data/maser/data/nancay/nda/data.py
+++ src/maser_data/maser/data/nancay/nda/data.py
@@ -26,3 +26,29 @@ class SrnNdaRoutineJupEdrCdfData(CdfData, dataset="srn_nda_routine_jup_edr"):
             with self.open(self.filepath) as f:
                 self._times = Time(f["Epoch"][...])
         return self._times
+
+    def as_xarray(self):
+        import xarray
+
+        dataset_keys = ["LL", "RR"]
+        datasets = {}
+
+        for dataset_key in dataset_keys:
+            datasets[dataset_key] = xarray.DataArray(
+                data=self.file[dataset_key][...].T,
+                name=self.file[dataset_key].attrs["LABLAXIS"],
+                coords=[
+                    (
+                        "frequency",
+                        self.frequencies.value,
+                        {"units": self.frequencies.unit},
+                    ),
+                    ("time", self.times.to_datetime()),
+                ],
+                dims=("frequency", "time"),
+                attrs={
+                    "unit": self.file[dataset_key].attrs["UNITS"],
+                    "title": self.file[dataset_key].attrs["CATDESC"],
+                },
+            )
+        return datasets
