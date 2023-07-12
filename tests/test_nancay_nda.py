7e9be6fd11416e7f690460e29a145aebc3adf781
Lucas <lucas.grosset@obspm.fr>
2023-07-12 16:32:19 +0200

    add tests for NDA Sun Routine

--- tests/test_nancay_nda.py
+++ tests/test_nancay_nda.py
@@ -5,6 +5,7 @@ from .constants import BASEDIR
 from maser.data import Data
 from maser.data.nancay import (
     OrnNdaRoutineJupEdrCdfData,
+    OrnNdaRoutineSunEdrCdfData,
     OrnNdaNewRoutineJupEdrFitsData,
 )
 import pytest
@@ -17,6 +18,12 @@ TEST_FILES = {
         / "nda"
         / "routine"
         / "srn_nda_routine_jup_edr_201601302247_201601310645_V12.cdf"
+    ],
+    "orn_nda_routine_sun_edr": [
+        BASEDIR
+        / "nda"
+        / "routine"
+        / "srn_nda_routine_sun_edr_202305231352_202305231534_V17.cdf"
     ],
     "orn_nda_newroutine_jup_edr": [
         BASEDIR
@@ -128,6 +135,100 @@ def test_orn_nda_routine_jup_edr_dataset_as_xarray():
 @pytest.mark.test_data_required
 def test_orn_nda_routine_jup_edr_dataset_quicklook():
     for filepath in TEST_FILES["orn_nda_routine_jup_edr"]:
+        ql_path = BASEDIR.parent / "quicklook" / "nda" / f"{filepath.stem}.png"
+        ql_path_tmp = Path("/tmp") / f"{filepath.stem}.png"
+        data = Data(filepath=filepath)
+        data.quicklook(ql_path_tmp)
+        assert open(ql_path, "rb").read() == open(ql_path_tmp, "rb").read()
+        ql_path_tmp.unlink()
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        data = Data(filepath=filepath)
+        assert isinstance(data, OrnNdaRoutineSunEdrCdfData)
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset__times():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        with Data(filepath=filepath) as data:
+            assert isinstance(data.times, Time)
+            assert len(data.times) == 6169
+            assert data.times[0] == Time("2023-05-23 13:52:11.60")
+            assert data.times[-1] == Time("2023-05-23 15:34:59.56")
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset__frequencies():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        with Data(filepath=filepath) as data:
+            assert isinstance(data.frequencies, Quantity)
+            assert len(data.frequencies) == 400
+            assert data.frequencies[0].to(Unit("MHz")).value == pytest.approx(10)
+            assert data.frequencies[-1].to(Unit("MHz")).value == pytest.approx(79.825)
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset__access_mode_file():
+    from spacepy import pycdf
+
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        with Data(filepath=filepath, access_mode="file") as data:
+            assert isinstance(data, pycdf.CDF)
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset__iter_method__mode_file():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        data = Data(filepath=filepath, access_mode="file")
+        var_labels = [item for item in data]
+        assert list(data.file) == var_labels
+        assert var_labels == [
+            "Epoch",
+            "Frequency",
+            "RR",
+            "LL",
+            "STATUS",
+            "SWEEP_TIME_OFFSET_RAMP",
+            "RR_SWEEP_TIME_OFFSET",
+        ]
+
+
+@pytest.mark.test_data_required
+@skip_if_spacepy_not_available
+def test_srn_nda_routine_sun_edr_dataset__access_mode_error():
+    with pytest.raises(ValueError):
+        Data(
+            filepath=TEST_FILES["orn_nda_routine_sun_edr"][0],
+            access_mode="toto",
+        )
+
+
+@pytest.mark.test_data_required
+def test_orn_nda_routine_sun_edr_dataset_as_xarray():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
+        data = Data(filepath=filepath)
+        xr = data.as_xarray()
+        assert isinstance(xr, dict)
+        assert set(xr.keys()) == {"LL", "RR"}
+        assert xr["LL"].shape == (400, 6169)  # 28734)
+        assert xr["LL"].attrs["units"] == "dB"
+        assert (
+            xr["LL"].attrs["title"]
+            == "Flux density spectrogram measured on the LH polarized array."
+        )
+
+
+@pytest.mark.test_data_required
+def test_orn_nda_routine_sun_edr_dataset_quicklook():
+    for filepath in TEST_FILES["orn_nda_routine_sun_edr"]:
         ql_path = BASEDIR.parent / "quicklook" / "nda" / f"{filepath.stem}.png"
         ql_path_tmp = Path("/tmp") / f"{filepath.stem}.png"
         data = Data(filepath=filepath)
